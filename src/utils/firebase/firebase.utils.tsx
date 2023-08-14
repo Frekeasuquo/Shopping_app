527e3ced9626bfdb21c019c97eaa8adc4dc7128f
Frekeasuquo <asuquondifreke06@gmail.com>
2023-08-14 19:06:51 +0100

    update

--- src/utils/firebase/firebase.utils.tsx
+++ src/utils/firebase/firebase.utils.tsx
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged  } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, User, NextOrObserver  } from 'firebase/auth';
 import {
   getFirestore,
   doc,
@@ -8,9 +8,10 @@ import {
   collection, // allow us to get a collection reference
   writeBatch, // use to perform write to a database
   query,
-  getDocs
+  getDocs,
+  QueryDocumentSnapshot
 } from 'firebase/firestore'
-
+import { Category } from '../../store/categories/category.types';
 
 
 // Your web app's Firebase configuration
@@ -36,20 +37,22 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
-
 export const db = getFirestore();
 
-export const addCollectionAndDocuments = async (
-  collectionKey, 
-  objectsToAdd, 
-  field = 'title' 
-  ) => {
+export type ObjectToAdd = {
+  title: string;
+}
+
+export const addCollectionAndDocuments = async <T extends ObjectToAdd> (
+  collectionKey: string, 
+  objectsToAdd: T[], 
+  ): Promise<void> => {
     const collectionRef = collection(db, collectionKey)
 
     const batch = writeBatch(db);
 
     objectsToAdd.forEach((object) => {
-      const docRef = doc(collectionRef, object[field].toLowerCase());
+      const docRef = doc(collectionRef, object.title.toLowerCase());
       batch.set(docRef, object);
     })
 
@@ -58,18 +61,31 @@ export const addCollectionAndDocuments = async (
   // Transaction is a word that represent a successful unit of work to a database, it might be writing mutiple/singe unit documet to a db.
 }
 
-export const getCategoriesAndDocuments = async () => {
+export const getCategoriesAndDocuments = async (): Promise<Category[]> => {
 
   const collectionRef = collection(db, 'categories');
   const q = query(collectionRef);
 
   // await Promise.reject(new Error('new error woops'))
   const querySnapshot = await getDocs(q);
-  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data() as Category );
 
 }
 
-export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
+export type AdditionalInformation = {
+  displayName?: string;
+}
+
+export type UserData = {
+  createdAt: Date;
+  displayName: string;
+  email: string;
+}
+
+export const createUserDocumentFromAuth = async (
+    userAuth: User, 
+    additionalInformation = {} as AdditionalInformation
+  ): Promise<void | QueryDocumentSnapshot<UserData>> => {
   if (!userAuth) return;
 
   const userDocRef = doc(db, 'users', userAuth.uid)
@@ -92,20 +108,20 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
         ...additionalInformation
       });
     } catch (error) {
-      console.log('Error creating the user', error.message);
+      console.log('Error creating the user', error);
     }
   }
 
-  return userSnapshot;
+  return userSnapshot as QueryDocumentSnapshot<UserData>;
 };
 
-export const createAuthUserWithEmailAndPassword = async (email, password) => {
+export const createAuthUserWithEmailAndPassword = async (email: string, password: string) => {
   if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password)
 }
 
-export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+export const signInAuthUserWithEmailAndPassword = async (email: string, password: string) => {
   if (!email || !password) return;
 
   return await signInWithEmailAndPassword(auth, email, password)
@@ -113,9 +129,9 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = async () => await signOut(auth)
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback,);
+export const onAuthStateChangedListener = (callback: NextOrObserver<User>) => onAuthStateChanged(auth, callback,);
 
-export const getCurrentUser = () => {
+export const getCurrentUser = (): Promise<User | null> => {
   return new Promise((resolve, reject) => {
     const unsubscribe = onAuthStateChanged(
       auth, 
